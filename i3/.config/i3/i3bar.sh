22f94010d90a379e0622cc23241dbbcbadabaf00
Goderich <yuhsien77@gmail.com>
2017-11-26 19:10:19 +0800

    Added battery info to bar and removed cbatticon

--- i3/.config/i3/i3bar.sh
+++ i3/.config/i3/i3bar.sh
@@ -3,6 +3,7 @@
 while :
 do
 	vol=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-echo	'Vol:' $vol'  ' $(date '+%a %d %b %T')
+	bat=$(awk -F", " '/Battery/ {print $2}' <(acpi -b))
+echo	'Charge:' $bat'  Vol:' $vol'  ' $(date '+%a %d %b %T')
 	sleep 1
 done
