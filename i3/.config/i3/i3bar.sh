3997b8069ead55c775627c72ed7e7d8b886e50cb
Goderich <yuhsien77@gmail.com>
2017-12-07 17:05:28 +0800

    Added "Muted" check to volume info in i3bar

--- i3/.config/i3/i3bar.sh
+++ i3/.config/i3/i3bar.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+function volume {
+	local amixer=$(awk '/dB/' <(amixer sget Master))
+	if echo $amixer | grep off > /dev/null; then
+		echo "Muted"
+	else
+		echo $amixer | awk -F"[][]" '{print $2}'
+	fi
+}
+
 while :
 do
-	vol=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
+	vol=$(volume)
 	bat=$(awk -F", " '/Battery/ {print $2}' <(acpi -b))
 echo	'Charge:' $bat'  Vol:' $vol'  ' $(date '+%a %d %b %T')
 	sleep 1
 done
+
