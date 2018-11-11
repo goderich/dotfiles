7365d105f6b077a97480fdb502e36520b3b14381
Goderich <yuhsien77@gmail.com>
2018-11-11 23:36:07 +0800

    Disable auto-fill-mode in org and markdown

--- doom-emacs/.config/doom/+ui.el
+++ doom-emacs/.config/doom/+ui.el
@@ -2,3 +2,8 @@
 
 ;; Soft wrap long lines
 (global-visual-line-mode 1)
+
+;; Disable auto-fill-mode
+(auto-fill-mode -1)
+(remove-hook 'markdown-mode-hook #'auto-fill-mode)
+(remove-hook 'org-mode-hook #'auto-fill-mode)
