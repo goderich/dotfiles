fbce15774ddab51d59b56bd633c6677a71752ec6
Goderich <yuhsien77@gmail.com>
2018-11-17 00:09:33 +0800

    Added localleader keybinds for org and mu4e

--- doom-emacs/.config/doom/+keybindings.el
+++ doom-emacs/.config/doom/+keybindings.el
@@ -1,7 +1,30 @@
 ;;; ~/dotfiles/doom-emacs/.config/doom/+keybindings.el -*- lexical-binding: t; -*-
 
-;; Set my keybindings
+;; Global keybindings
+
+;; comma as localleader is baws
+(setq doom-localleader-key ",")
+
 (map!
  (:leader
    (:prefix "t"
-   :desc "Toggle writeroom mode" :n "w" 'writeroom-mode)))
+    :desc "Toggle writeroom mode" :n "w" 'writeroom-mode)))
+
+;; org-mode keybindings
+
+(after! org
+  (map! :map evil-org-mode-map
+        :localleader
+        :desc "Open link" :nv "l" 'org-open-at-point
+        :desc "org-todo" :nv "t" 'org-todo))
+
+
+;; mu4e keybindings
+
+(after! mu4e-compose
+  (map! :map mu4e-compose-mode-map
+        :localleader
+        :desc "Cancel and delete" :nv "k" 'mu4e-message-kill-buffer
+        :desc "Add attachment" :nv "a" 'mail-add-attachment
+        :desc "Send" :nv "s" 'message-send
+        ))
