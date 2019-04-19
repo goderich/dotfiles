e528442399dcfdb57363bae9a6afca32b6d25157
Goderich <yuhsien77@gmail.com>
2019-04-19 20:13:42 +0800

    Added bibliography location for org-ref

---
+++ doom-emacs/.config/doom/+referencing.el
@@ -0,0 +1,5 @@
+;;; ~/dotfiles/doom-emacs/.config/doom/+referencing.el -*- lexical-binding: t; -*-
+
+(setq reftex-default-bibliography "~/pap.bib")
+(setq org-ref-default-bibliography reftex-default-bibliography)
+(setq bibtex-completion-bibliography reftex-default-bibliography)
