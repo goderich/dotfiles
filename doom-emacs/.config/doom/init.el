3a90af10d016a55414955eb4cffb1b1928d3097f
Goderich <yuhsien77@gmail.com>
2019-05-03 14:03:56 +0800

    Add pdf layer

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -72,7 +72,7 @@
        ;;make              ; run make tasks from Emacs
        magit             ; a git porcelain for Emacs
        ;;password-store    ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
