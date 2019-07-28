14d5ed7ce002b5a92d2c561ac86dbf5769aeaf22
Goderich <yuhsien77@gmail.com>
2019-07-28 20:46:25 +0800

    Add mail config

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -10,6 +10,7 @@
 (load! "+ui")
 (load! "+keybindings")
 (load! "+referencing")
+(load! "+mail")
 
 ;; Disable company-mode in org and markdown
 (setq company-global-modes '(not org-mode markdown-mode))
