989aec737c0b2aeb37d135e8b0834bc97ebcc4bf
Goderich <yuhsien77@gmail.com>
2019-09-24 14:06:11 +0800

    Nuke all mail-related stuff from emacs
    I'm now using neomutt for mail. I tried experimenting with mu4e, but
    found that I prefer neomutt after all.

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -10,7 +10,6 @@
 (load! "+ui")
 (load! "+keybindings")
 (load! "+referencing")
-(load! "+mail")
 
 ;; Disable company-mode in org and markdown
 (setq company-global-modes '(not org-mode markdown-mode))
