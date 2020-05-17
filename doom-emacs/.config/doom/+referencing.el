8fae3c3db2000fa5c24ed2abc99c510fee210060
Goderich <yuhsien77@gmail.com>
2020-05-17 17:19:46 +0800

    emacs: change formatting of helm-bibtex list
    The formatting part was easy, but it took a bit to figure out which
    package to put in (after!), because the setq wasn't running.
    
    In the end it was `org-ref-helm-bibtex`, the package that contains the
    function I use to insert citations.

--- doom-emacs/.config/doom/+referencing.el
+++ doom-emacs/.config/doom/+referencing.el
@@ -3,3 +3,8 @@
 (setq reftex-default-bibliography "~/pap.bib")
 (setq org-ref-default-bibliography reftex-default-bibliography)
 (setq bibtex-completion-bibliography reftex-default-bibliography)
+
+; Formatting of helm-bibtex results
+(after! org-ref-helm-bibtex
+  (setq bibtex-completion-display-formats
+        '((t . "${author:18} ${year:5} ${title:*}"))))
