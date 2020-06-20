b4486bd6a1931b67f7204acefc22ae47b68cf147
Goderich <yuhsien77@gmail.com>
2020-06-20 14:08:38 +0800

    emacs: doom package changes

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -103,11 +103,11 @@
        ;;julia             ; a better, faster MATLAB
        latex             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+       ;; lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
+       ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         ;;+attach          ; custom attachment system
         +babel           ; running code in org
@@ -120,7 +120,7 @@
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        ;; qt                ; the 'cutest' gui framework ever
-       racket            ; a DSL for DSLs
+       ;; racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;; rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
