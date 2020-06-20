aa76e1c7d287d01e26a2615a9b3a424a00384a7d
Goderich <yuhsien77@gmail.com>
2020-06-20 14:10:04 +0800

    fish: automatic config for opam

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,5 @@ set -gx EDITOR /usr/bin/nvim
 set -gx BROWSER /usr/bin/qutebrowser
 set -gx PAGER /usr/bin/less
 
+# opam configuration
+source /home/iwaka/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
