14790415e737c14bd23dc47e6b012e0e2c671d87
Goderich <yuhsien77@gmail.com>
2020-07-08 16:17:39 +0800

    emacs: clean up and fix org config
    No new functionality, but I cleaned it up a little, removing some
    superfluous `after!` declarations and putting everything under a single
    macro. Changed the order and indentation of a few things to make it more
    readable and in general just nicer.
    
    Also moved the blank lines after headers `setq` inside the `after!`
    block because it wasn't working otherwise. Works now tho.

--- doom-emacs/.config/doom/+org.el
+++ doom-emacs/.config/doom/+org.el
@@ -5,34 +5,30 @@
  '(org-directory "~/Dropbox/org")
  '(org-agenda-files (list org-directory)))
 
-;; Use blank lines between texts and following headings
-(setq org-blank-before-new-entry
-      '((heading . t) (plain-list-item . t)))
-
-;; Set org file associations
 (after! org
-  ;; browser needs to be set with a separate function
-  (setq browse-url-browser-function 'browse-url-generic
-        browse-url-generic-program "qutebrowser")
 
+  ;; Set org file associations
   (setq org-file-apps
       '((auto-mode . emacs)
       ("\\.pdf::\\([0-9]+\\)?\\'" . "zathura %s -P %1")
       ("\\.pdf\\'" . "zathura %s")
-      (directory . emacs))))
+      (directory . emacs)))
 
-;; Org todo keywords and colours
-(after! org
+  ;; browser needs to be set with a separate function
+  (setq browse-url-browser-function 'browse-url-generic
+        browse-url-generic-program "qutebrowser")
+
+  ;; Use blank lines between texts and following headings
+  (setq org-blank-before-new-entry
+        '((heading . t) (plain-list-item . t)))
+
+  ;; Org todo keywords and colours
   (setq org-todo-keywords
         '((sequence "TODO(t)" "|" "DONE(d)")
           (sequence "NEXT(n)" "WAITING(w)" "LATER(l)" "|" "CANCELLED(c)")))
 
   (setq org-todo-keyword-faces
-        '(("LATER" :inherit
-           (warning bold))
-          ("NEXT" :inherit
-           (bold default))
-          ("WAITING" :inherit
-           (warning bold))
-          ("CANCELLED" :inherit
-           (error bold)))))
+        '(("LATER" :inherit (warning bold))
+          ("NEXT" :inherit (bold default))
+          ("WAITING" :inherit (warning bold))
+          ("CANCELLED" :inherit (error bold)))))
