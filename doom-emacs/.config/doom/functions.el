fa9bd5c193774d37dc6a2ac876c23d14213f4908
Goderich <yuhsien77@gmail.com>
2020-08-02 16:21:02 +0800

    Add Markdown table to LaTeX conversion function
    It's by no means perfect, but it has worked for my needs, and it's worth
    saving for later use.

--- doom-emacs/.config/doom/functions.el
+++ doom-emacs/.config/doom/functions.el
@@ -22,3 +22,74 @@
   (beginning-of-line)
   (dotimes (i (string-to-number numlines))
     (insert (format "%d\n" (1+ i)))))
+
+(defun md-table->latex ()
+  "Convert a Markdown table preceding the point into LaTeX."
+  (interactive)
+  (defun convert-row ()
+    (let ((md-asterisk (rx "\\" "\*"))
+          (md-bold (rx (repeat 2 "\*")
+                       word-start
+                       (group (minimal-match (zero-or-more (not (any "\&")))))
+                       word-end
+                       (repeat 2 "\*")))
+          (md-opening-quote (rx " '" word-start)))
+      (delete-char 2)
+      (while (re-search-forward "|" (1- (line-end-position)) t)
+        (replace-match "&"))
+      (re-search-forward "|")
+      (replace-match "\\\\ \\addlinespace" t t)
+      (beginning-of-line)
+      (while (re-search-forward md-asterisk (1- (line-end-position)) t)
+        (replace-match "\\\\text{\*}"))
+      (beginning-of-line)
+      (while (re-search-forward md-bold (1- (line-end-position)) t)
+        (replace-match "\\\\textbf{\\1}"))
+      (beginning-of-line)
+      (while (re-search-forward md-opening-quote (1- (line-end-position)) t)
+        (replace-match " \`"))
+      (forward-line)))
+  (progn
+    (re-search-backward "^:")
+    (skip-chars-forward "[: ]")
+    (setq table-caption
+          (replace-regexp-in-string
+           "\\\\\\*"  "\\\\text{\*}"
+           (delete-and-extract-region
+            (point)
+            (- (search-forward " {#") 3))))
+    (setq table-label
+          (delete-and-extract-region
+           (point)
+           (1- (line-end-position))))
+    (beginning-of-line)
+    (delete-region (point) (progn (forward-line 1) (point)))
+    (insert
+     "\\begin{table}[hbt!]\n"
+     "\\centering\n"
+     "\\caption{" table-caption "}\n"
+     "\\label{" table-label "}\n")
+    (setq table-top (point))
+    (forward-line)
+    (setq table-columns
+          (1- (cl-count ?| (thing-at-point 'line))))
+    (convert-row)
+    (re-search-backward "addlinespace")
+    (replace-match "midrule \\\\addlinespace")
+    (forward-line)
+    (delete-region (point) (progn (forward-line 1) (point)))
+    (while (< 0 (cl-count ?| (thing-at-point 'line)))
+      (convert-row))
+    (setq table-bottom (point))
+    (re-search-backward "addlinespace")
+    (replace-match "bottomrule")
+    (forward-line)
+    (insert
+     "\\end{tabular}\n"
+     "\\end{table}\n")
+    (goto-char table-top)
+    (insert
+     "\\begin{tabular}{ "
+     (apply 'concat (make-list table-columns "l "))
+     "} \\toprule")
+    (re-search-backward "]")))
