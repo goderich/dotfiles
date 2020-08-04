1366e735b2270436ceb04d68c072fd55c1590d87
Goderich <yuhsien77@gmail.com>
2020-08-05 01:09:41 +0800

    emacs: rehaul mu4e config
    I decided to commit this all at once instead of a bunch of atomic
    changes.
    
    I changed many things, from comments and indenting, to moving things
    around and refactoring, to adding some new stuff (mostly related to
    visual-fill-column-mode).

--- doom-emacs/.config/doom/+mail.el
+++ doom-emacs/.config/doom/+mail.el
@@ -1,63 +1,60 @@
 ;;; ~/dotfiles/doom-emacs/.config/doom/+mail.el -*- lexical-binding: t; -*-
 (require 'mu4e)
 
-;; use mu4e for e-mail in emacs
+;; Use mu4e for e-mail in emacs
 (setq mail-user-agent 'mu4e-user-agent)
 
-(setq mu4e-maildir "~/.mail")
 (setq mu4e-drafts-folder "/bak.drafts")
 (setq mu4e-sent-folder   "/bak.sent")
 (setq mu4e-trash-folder  "/bak.trash")
 
-;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+;; Don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq mu4e-sent-messages-behavior 'delete)
-
 ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
 ;; additional non-Gmail addresses and want assign them different
 ;; behavior.)
 
-;; setup some handy shortcuts
-;; you can quickly switch to your Inbox -- press ``ji''
+;; Setup some handy shortcuts
+;; You can quickly switch to your Inbox -- press ``ji''
 ;; then, when you want archive some messages, move them to
 ;; the 'All Mail' folder by pressing ``ma''.
-
 (setq mu4e-maildir-shortcuts
-    '( ("/inbox"      . ?i)
-       ("/bak.sent"   . ?s)
-       ("/bak.trash"  . ?t)))
+    '(("/inbox"      . ?i)
+      ("/bak.sent"   . ?s)
+      ("/bak.trash"  . ?t)))
 
-;; allow for updating mail using 'u' in the main view:
+;; Allow for updating mail using 'u' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
 
-;; something about ourselves
-(setq
-   user-mail-address "yuhsien77@gmail.com"
-   user-full-name  "Andre Goderich 郭育賢")
+;; My credentials
+(setq user-mail-address "yuhsien77@gmail.com"
+      user-full-name  "郭育賢 Andre Goderich")
 
-;; sending mail -- replace USERNAME with your gmail username
-;; also, make sure the gnutls command line utils are installed
-;; package 'gnutls-bin' in Debian/Ubuntu
+;; Sending mail
 (require 'smtpmail)
 (setq message-send-mail-function 'smtpmail-send-it
-    smtpmail-stream-type 'starttls
-    smtpmail-default-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-service 587)
+      smtpmail-stream-type 'starttls
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      mu4e-compose-format-flowed nil
+      mu4e-compose-dont-reply-to-self t)
 
-;; don't keep message buffers around
+;; Don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
 
-;; don't use org-mode to compose email
-(setq mu4e-compose-mode-hook
-      '(flyspell-mode))
+;; Don't use org-mode to compose email
+(remove-hook 'mu4e-compose-mode-hook #'org-mu4e-compose-org-mode)
 
-;; enable automatic wrapping of emails
-(add-hook 'message-mode-hook 'auto-fill-mode)
-(setq fill-column 65
-      mu4e-compose-dont-reply-to-self t
-      mu4e-compose-format-flowed t)
+;; Visually wrap lines at 80 colums when composing.
+;; I have tried format=flowing before, but any shenanigans
+;; with line wrapping break in Gmail. The only way to have
+;; emails display decently on different devices is to send
+;; them in long lines. Editing these is a pain though, hence
+;; this setting.
+(add-hook 'mu4e-compose-mode-hook
+          #'visual-fill-column-mode
+          :local (setq fill-column 80))
+
 ;; optional:
 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
-
-;; use messages-are-flowing package
-(add-hook 'message-mode-hook 'messages-are-flowing-use-and-mark-hard-newlines)
