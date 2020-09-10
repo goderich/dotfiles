b08a7d15ab6179aa9797b68b7c282748fc2652b9
Goderich <yuhsien77@gmail.com>
2020-09-10 17:17:23 +0800

    ebib: configure index window UI

--- doom-emacs/.config/doom/+referencing.el
+++ doom-emacs/.config/doom/+referencing.el
@@ -23,4 +23,13 @@
   (setq ebib-preload-bib-files `(,my/default-bibliography))
   (setq ebib-file-associations '(("pdf" . "zathura") ("ps" . "gv")))
   (setq ebib-notes-directory my/ebib-notes)
-  (setq ebib-bibtex-dialect 'biblatex))
+  (setq ebib-bibtex-dialect 'biblatex)
+  (setq ebib-index-columns '(("Author/Editor" 20 t)
+                             ("Year" 6 t)
+                             ("Title" 40 t))))
+
+; Visual line mode makes entries occupy 2 or even more lines in the index.
+; This is unnecessary, because I can see the full list of authors and title
+; right below in the entry window. It's better to see more entries at the
+; same time in the index window.
+(add-hook 'ebib-index-mode-hook (lambda () (visual-line-mode -1)))
