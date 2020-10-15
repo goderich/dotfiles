4a0980a3b2bdc1d1a49d9560ccea70a07f7f0952
Goderich <yuhsien77@gmail.com>
2020-10-15 14:10:39 +0800

    emacs: add darcula theme package

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -9,3 +9,7 @@
 
 ;; Nested alist manipulation
 (package! let-alist)
+
+;; jetbrains darcula theme
+(package! jetbrains-darcula-theme
+  :recipe (:host github :repo "ianpan870102/jetbrains-darcula-emacs-theme"))
