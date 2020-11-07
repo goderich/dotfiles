6fd872262f5f7a73df11ee0f2d0f355da33141b6
Goderich <yuhsien77@gmail.com>
2020-11-07 12:43:32 +0800

    Use racket-xp-mode
    Had this off while I was figuring out a bug, see:
    https://github.com/greghendershott/racket-mode/issues/504
    
    Now that there is a solution, I can safely turn it on and enjoy the
    goodness.

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -157,7 +157,8 @@
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;; racket            ; a DSL for DSLs
+       (racket            ; a DSL for DSLs
+        +xp)
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
