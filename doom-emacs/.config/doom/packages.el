29486aec81960ca6e3eb0cc4bfc505c76243c0a4
Goderich <yuhsien77@gmail.com>
2020-11-20 15:20:46 +0800

    Remove darcula theme

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -9,7 +9,3 @@
 
 ;; Nested alist manipulation
 (package! let-alist)
-
-;; jetbrains darcula theme
-(package! jetbrains-darcula-theme
-  :recipe (:host github :repo "ianpan870102/jetbrains-darcula-emacs-theme"))
