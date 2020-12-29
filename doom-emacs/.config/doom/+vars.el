1bffb6ab7a05e7283a4c92a761fedd77af5c632e
Goderich <yuhsien77@gmail.com>
2020-12-29 15:15:03 +0800

    Refactor choose-vars macro
    Instead of taking a single choice and writing the macro out for each
    variable, I modified it to take a list of choices, so I can put all the
    vars into one macro.

--- doom-emacs/.config/doom/+vars.el
+++ doom-emacs/.config/doom/+vars.el
@@ -2,13 +2,22 @@
 
 ;; Define variables for various machines
 
-(defmacro choose-var (var x y)
-  "Use X if on laptop, otherwise Y."
-  `(defvar ,var
-        (if (string= (system-name) "iwaka-thinkpad")
-            ,x ,y)))
+(defun choose-var-helper (vars)
+  "Comb VARS for three variables: VAR, X, and Y.
+Assign X to VAR if on laptop, otherwise assign Y to VAR."
+  (let ((var (car vars))
+        (x (cadr vars))
+        (y (caddr vars)))
+    `(defvar ,var
+       (if (string= (system-name) "iwaka-thinkpad")
+           ,x ,y))))
 
-(choose-var my/org-directory "~/Dropbox/org"  "~/Documents/org")
-(choose-var my/default-bibliography "~/Dropbox/pap.bib" "~/Documents/bibliography.bib")
-(choose-var my/org-roam-directory "~/Dropbox/org/roam"  "~/Documents/org/roam")
-(choose-var my/ebib-notes "~/Dropbox/org/literature-notes"  "~/Documents/org/literature-notes")
+(defmacro choose-vars (lst)
+  "Use X if on laptop, otherwise Y.
+Takes a list of (VAR X Y) lists as an argument."
+  (macroexp-progn (mapcar #'choose-var-helper lst)))
+
+(choose-vars ((my/org-directory "~/Dropbox/org"  "~/Documents/org")
+              (my/default-bibliography "~/Dropbox/pap.bib" "~/Documents/bibliography.bib")
+              (my/org-roam-directory "~/Dropbox/org/roam"  "~/Documents/org/roam")
+              (my/ebib-notes "~/Dropbox/org/literature-notes"  "~/Documents/org/literature-notes")))
