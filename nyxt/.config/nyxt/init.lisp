f799bb2051357808c7c78b7ce9a8edb461a2906b
Goderich <yuhsien77@gmail.com>
2021-03-24 16:59:42 +0800

    Bump Nyxt config
    With the Nyxt 2 pre-release #6, some APIs changed, so I had to rename a
    function in my config (namely `reload-buffer`). I took this opportunity
    to also clean the config up a bit, and align it.

--- nyxt/.config/nyxt/init.lisp
+++ nyxt/.config/nyxt/init.lisp
@@ -2,17 +2,17 @@
 
 (defvar *my-keymap* (make-keymap "my-map"))
 (define-key *my-keymap*
-  "down" 'nyxt/web-mode:scroll-down
-  "up" 'nyxt/web-mode:scroll-up
-  "h" 'nyxt/web-mode:follow-hint
-  "H" 'nyxt/web-mode:follow-hint-new-buffer
-  "f" 'nyxt/web-mode:history-backwards
-  "?" 'nyxt/web-mode:search-buffers
-  "b b" 'switch-buffer
-  "r" 'reload-current-buffer
-  "R" 'reload-buffer
-  "; d" 'nyxt/web-mode:download-hint-url
-  "; y" 'nyxt/web-mode:copy-hint-url
+  "down"   'nyxt/web-mode:scroll-down
+  "up"     'nyxt/web-mode:scroll-up
+  "h"      'nyxt/web-mode:follow-hint
+  "H"      'nyxt/web-mode:follow-hint-new-buffer
+  "f"      'nyxt/web-mode:history-backwards
+  "?"      'nyxt/web-mode:search-buffers
+  "b b"    'switch-buffer
+  "r"      'reload-current-buffer
+  "R"      'reload-buffers
+  "; d"    'nyxt/web-mode:download-hint-url
+  "; y"    'nyxt/web-mode:copy-hint-url
   "escape" 'nyxt/web-mode:remove-search-hints)
 
 (define-mode my-mode ()
@@ -23,4 +23,4 @@
                    scheme:vi-normal *my-keymap*))))
 
 (define-configuration (buffer web-buffer)
-  ((default-modes (append '(my-mode vi-normal-mode) %slot-default))))
+  ((default-modes (append '(my-mode) %slot-default))))
