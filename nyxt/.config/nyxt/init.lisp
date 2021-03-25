0a1eb5e8ce674238d54afccbd5881eeac9939a42
Goderich <yuhsien77@gmail.com>
2021-03-25 20:12:23 +0800

    Add prompt-buffer:style config
    Changed the font size and the column width in the prompt.

--- nyxt/.config/nyxt/init.lisp
+++ nyxt/.config/nyxt/init.lisp
@@ -27,3 +27,25 @@
 
 (define-configuration browser
   ((session-restore-prompt :always-restore)))
+
+(define-configuration prompt-buffer
+  ((style
+"* { font-family: monospace,monospace; font-size: 18px; line-height: 18px; }
+body { overflow: hidden; margin: 0; padding: 0; }
+#prompt-area { background-color: dimgray; display: grid; grid-template-columns: auto auto 1fr; width: 100%; color: white; }
+#prompt { padding-left: 10px; line-height: 26px; }
+#prompt-extra { line-height: 26px; padding-right: 7px; }
+#input { border: none; outline: none; padding: 3px; background-color: #E8E8E8; width: 100%; autofocus: true; }
+.source { margin-left: 10px; margin-top: 15px; }
+.source-glyph { margin-right: 3px; }
+.source-name { color: white; padding-left: 5px; line-height: 24px; background-color: gray; }
+#suggestions { overflow-y: hidden; overflow-x: hidden; height: 100%; width: 100%; }
+.source-content { margin-left: 10px; background-color: #F7F7F7; width: 100%; table-layout: fixed; }
+.source-content th:first-child { width: 20%; }
+.source-content th:nth-child(2) { width: 20%; }
+.source-content td { white-space: nowrap; height: 20px; overflow: auto; }
+.source-content th { font-weight: normal; padding-left: 3px; text-align: left; background-color: #E8E8E8; }
+.source-content td::-webkit-scrollbar { display: none; }
+#selection { background-color: 575757; color: white; }
+.marked { background-color: darkgray; font-weight: bold; color: white; }
+.selected { background-color: gray; color: white; }")))
