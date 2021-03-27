fe419f404cd4a79346534454e9ec6e87bd267f2c
Andre Goderich <goderich@goderich-ncue.localdomain>
2021-03-27 22:14:16 +0800

    Redefine org variables
    I used to have these in separate places on my laptop vs my work machine.
    Now, however, they are both in the same place on Dropbox, at least for
    the time being.

--- doom-emacs/.config/doom/vars.el
+++ doom-emacs/.config/doom/vars.el
@@ -18,7 +18,7 @@ Assign X to VAR if on laptop, otherwise assign Y to VAR."
 Takes a list of (VAR X Y) lists as an argument."
   (macroexp-progn (mapcar #'choose-var-helper lst)))
 
-(choose-vars ((my/org-directory "~/Dropbox/org"  "~/Documents/org")
-              (my/default-bibliography "~/Dropbox/pap.bib" "~/Documents/bibliography.bib")
-              (my/org-roam-directory "~/Dropbox/org/roam"  "~/Documents/org/roam")
-              (my/ebib-notes "~/Dropbox/org/literature-notes"  "~/Documents/org/literature-notes")))
+(setq my/org-directory "~/Dropbox/org"
+      my/default-bibliography "~/Dropbox/pap.bib"
+      my/org-roam-directory "~/Dropbox/org/roam"
+      my/ebib-notes "~/Dropbox/org/literature-notes")
