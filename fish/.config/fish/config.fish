3d2d828e0ce588bc3aea1ea585276e63150314df
Goderich <yuhsien77@gmail.com>
2021-07-01 21:15:34 +0800

    Do not set PATH in config.fish
    Fish has other methods to do this.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,6 @@
 set fish_greeting
 
 # Set environment variables
-set -gx PATH $PATH $HOME/.local/bin
 set -gx EDITOR /usr/bin/nvim
 set -gx BROWSER /usr/bin/qutebrowser
 set -gx PAGER /usr/bin/less
