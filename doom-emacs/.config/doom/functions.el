a1d57f9a78bf6f9d3a436fd7202934f3cb74e999
Goderich <yuhsien77@gmail.com>
2022-11-03 19:45:31 +0800

    Purge stale functions from config

--- doom-emacs/.config/doom/functions.el
+++ doom-emacs/.config/doom/functions.el
@@ -25,77 +25,6 @@ amount of lines to create."
     (dotimes (i numlines)
       (insert (format "%d\n" (1+ i))))))
 
-(defun md-table->latex ()
-  "Convert a Markdown table preceding the point into LaTeX."
-  (interactive)
-  (defun convert-row ()
-    (let ((md-asterisk (rx "\\" "\*"))
-          (md-bold (rx (repeat 2 "\*")
-                       word-start
-                       (group (minimal-match (zero-or-more (not (any "\&")))))
-                       word-end
-                       (repeat 2 "\*")))
-          (md-opening-quote (rx " '" word-start)))
-      (delete-char 2)
-      (while (re-search-forward "|" (1- (line-end-position)) t)
-        (replace-match "&"))
-      (re-search-forward "|")
-      (replace-match "\\\\ \\addlinespace" t t)
-      (beginning-of-line)
-      (while (re-search-forward md-asterisk (1- (line-end-position)) t)
-        (replace-match "\\\\text{\*}"))
-      (beginning-of-line)
-      (while (re-search-forward md-bold (1- (line-end-position)) t)
-        (replace-match "\\\\textbf{\\1}"))
-      (beginning-of-line)
-      (while (re-search-forward md-opening-quote (1- (line-end-position)) t)
-        (replace-match " \`"))
-      (forward-line)))
-  (progn
-    (re-search-backward "^:")
-    (skip-chars-forward "[: ]")
-    (setq table-caption
-          (replace-regexp-in-string
-           "\\\\\\*"  "\\\\text{\*}"
-           (delete-and-extract-region
-            (point)
-            (- (search-forward " {#") 3))))
-    (setq table-label
-          (delete-and-extract-region
-           (point)
-           (1- (line-end-position))))
-    (beginning-of-line)
-    (delete-region (point) (progn (forward-line 1) (point)))
-    (insert
-     "\\begin{table}[hbt!]\n"
-     "\\centering\n"
-     "\\caption{" table-caption "}\n"
-     "\\label{" table-label "}\n")
-    (setq table-top (point))
-    (forward-line)
-    (setq table-columns
-          (1- (cl-count ?| (thing-at-point 'line))))
-    (convert-row)
-    (re-search-backward "addlinespace")
-    (replace-match "midrule \\\\addlinespace")
-    (forward-line)
-    (delete-region (point) (progn (forward-line 1) (point)))
-    (while (< 0 (cl-count ?| (thing-at-point 'line)))
-      (convert-row))
-    (setq table-bottom (point))
-    (re-search-backward "addlinespace")
-    (replace-match "bottomrule")
-    (forward-line)
-    (insert
-     "\\end{tabular}\n"
-     "\\end{table}\n")
-    (goto-char table-top)
-    (insert
-     "\\begin{tabular}{ "
-     (apply 'concat (make-list table-columns "l "))
-     "} \\toprule")
-    (re-search-backward "]")))
-
 (after! ebib
   ; These functions need to be loaded after ebib,
   ; which itself doesn't get loaded until it is called manually.
@@ -241,29 +170,6 @@ headings of a higher level. Use instead of mashing Alt+up."
         (message "Copied link!"))
     (message "Not on a valid link!")))
 
-;; This function is adapted from
-;; https://www.reddit.com/r/emacs/comments/nvhb82/can_i_use_racket_like_i_use_elisp/h13ug8i/
-(defun vz/racket-eros-eval-last-sexp ()
-  "Eval the previous sexp asynchronously and create an eros overlay."
-  (interactive)
-  (unless (racket--repl-live-p)
-    (user-error "No repl session available."))
-  (racket--cmd/async
-   (racket--repl-session-id)
-   `(eval ,(buffer-substring-no-properties (racket--repl-last-sexp-start) (point)))
-   (lambda (result)
-     (let ((point (point))
-           ;; Using :duration 'command (which is my current value of
-           ;; `eros-eval-result-duration') didn't work without setting
-           ;; `this-command'. The variable got nulled somewhere during
-           ;; the call, and eros would delete the overlay before even
-           ;; displaying it.
-           (this-command 'vz/racket-eros-eval-last-sexp))
-       (message "%s" result)
-       (eros--make-result-overlay result
-         :where point
-         :duration eros-eval-result-duration)))))
-
 (defun gd/set-racket-do-indent ()
   "Set up indentation for `do' in Racket."
   (put 'do 'racket-indent-function 0))
@@ -276,14 +182,6 @@ non-blank character instead."
   (if (bolp)
       (evil-first-non-blank)
     (evil-beginning-of-line)))
-
-(defun gd/scroll-line-to-top-minus-2 ()
-  "Scroll line almost to the very top, but not quite."
-  (interactive)
-  (save-excursion
-    (-> (line-number-at-pos)
-        (- 2)
-        (evil-scroll-line-to-top))))
 
 (defun gd/insert-link (address &optional name)
   "Insert an Org link to ADDRESS.
@@ -417,29 +315,6 @@ tags."
   "Submit this file to exercism."
   (interactive)
   (call-process "exercism" nil nil nil "submit" (f-this-file)))
-
-(defun gd/wlist->table ()
-  (save-restriction
-    (narrow-to-region (point-at-bol) (point-at-eol))
-    (beginning-of-line)
-    (push-mark)
-    (end-of-line)
-    (insert "||")
-    (goto-char (point-min))
-    (while (search-forward "、" nil t)
-      (replace-match "||\n|"))
-    (pop-mark)
-    (goto-char (mark))
-    (delete-char 2)
-    (insert "|")))
-
-(defun gd/tableize-region (beg end)
-  (interactive "r")
-  (save-restriction
-    (narrow-to-region beg end)
-    (goto-char (point-min))
-    (while (search-forward "•" nil t)
-      (gd/wlist->table))))
 
 (defun gd/consult-goto-org-heading ()
   "Find an org heading in the current buffer, and open it.
