0794c53f01ce763993e8780f27871f355a799521
Goderich <yuhsien77@gmail.com>
2023-01-10 16:18:15 +0800

    Enhance org-link-dwim
    Added functionality to store link when on a heading.

--- doom-emacs/.config/doom/functions.el
+++ doom-emacs/.config/doom/functions.el
@@ -333,12 +333,15 @@ tags."
     (message "Select a region with visual mode first!")))
 
 (defun gd/org-link-dwim ()
-  "If on a link, open it. If not, insert last stored link.
+  "Do-what-I-mean for linking.
+If on a heading, link it. If on a link, open it.
+Elsewhere, insert last stored link.
 This is a convenience function to bind it to a single keystroke."
   (interactive)
-  (if (org-in-regexp org-link-bracket-re 1)
-      (org-open-at-point)
-    (org-insert-last-stored-link 1)))
+  (cond
+   ((org-at-heading-p) (org-store-link nil))
+   ((org-in-regexp org-link-bracket-re 1) (org-open-at-point))
+   (t (org-insert-last-stored-link 1))))
 
 (defun gd/mu4e-link-dwim ()
   "If on a link, open it. If not, store the current message as link.
