6fbb6c2e936f525ab57ae9d9b8f83865efb6719a
Goderich <yuhsien77@gmail.com>
2023-02-22 22:58:15 +0800

    Add documentation to stretch filter

--- pandoc/.local/share/pandoc/lua-filters/stretch.lua
+++ pandoc/.local/share/pandoc/lua-filters/stretch.lua
@@ -1,3 +1,12 @@
+--[[
+All org-mode images get wrapped in a Para
+when parsed by pandoc. We don't want that,
+because the Revealjs .r-stretch class only
+works on *direct* descendants of the heading.
+Since we add the .r-stretch class here, it
+doesn't need to be added manually in org-mode
+(so you can just use a bare image).
+]]
 function Para(elem)
   if elem.c[1].t == "Image" then
     table.insert(elem.c[1].classes, "r-stretch")
