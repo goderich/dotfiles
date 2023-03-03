2a32efe14f1005b290bbce92ffea5fd1967a70ec
Goderich <yuhsien77@gmail.com>
2023-03-03 16:23:58 +0800

    Refactor stretch filter

--- pandoc/.local/share/pandoc/lua-filters/stretch.lua
+++ pandoc/.local/share/pandoc/lua-filters/stretch.lua
@@ -7,9 +7,10 @@ Since we add the .r-stretch class here, it
 doesn't need to be added manually in org-mode
 (so you can just use a bare image).
 ]]
-function Para(elem)
-  if elem.c[1].t == "Image" then
-    table.insert(elem.c[1].classes, "r-stretch")
-    return elem.c[1]
+function Para(p)
+  local el = p.c[1]
+  if el.t == "Image" then
+    table.insert(el.classes, "r-stretch")
+    return el
   end
 end
