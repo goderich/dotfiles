9fd4579e17792438db8a9c4c898d8d46e1f6e9b5
Goderich <yuhsien77@gmail.com>
2023-05-25 15:14:28 +0800

    Add comments to animate.lua

--- pandoc/.local/share/pandoc/lua-filters/animate.lua
+++ pandoc/.local/share/pandoc/lua-filters/animate.lua
@@ -1,7 +1,17 @@
+--[[
+This is for using org syntax to add RevealJS animations
+(see https://revealjs.com/auto-animate/).
+The syntax is a simple :animate: tag on the heading.
+Note that right now there can only be one tag per heading:
+if there is a second one, it will overwrite the first.
+]]
 function Header(h)
   local c = h.content
-  if c[#c].attr and
+  if c[#c].attr and -- null check, otherwise filter will crash
       c[#c].attributes["tag-name"] == "animate" then
+    -- For some reason tags in headings get interpreted as SmallCaps
+    -- and appear in the generated file. We don't want that,
+    -- so we delete the last two items in the heading (tag and preceding space).
     table.remove(c, #c)
     table.remove(c, #c)
     h.attributes["auto-animate"] = "true"
