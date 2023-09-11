6097fb2cd9e9d13adf84c2a2a72cea01a63d88ad
Goderich <yuhsien77@gmail.com>
2023-09-11 20:02:10 +0800

    Update `flac-to-mp3`

--- fish/.config/fish/functions/flac-to-mp3.fish
+++ fish/.config/fish/functions/flac-to-mp3.fish
@@ -3,7 +3,10 @@ function flac-to-mp3
   echo "Converting" (count **/*.{flac,FLAC}) "files..."
   fd --type f --extension flac --exec \
       ffmpeg -i "{}" -codec:a libmp3lame -qscale:a 2 \
+      # pass cover images without conversion
+      -fps_mode passthrough \
       # run quietly, but show conversion progress
-      -loglevel warning -stats \
+      -loglevel warning \
+      -stats \
       "{.}.mp3"
 end
