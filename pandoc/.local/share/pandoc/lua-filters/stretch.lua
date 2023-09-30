8c52141a306a450de736ab6738189f9a66e651c4
Goderich <yuhsien77@gmail.com>
2023-09-30 14:49:52 +0800

    pandoc: update stretch.lua filter
    Now I can control which images I do not want stretched.

--- pandoc/.local/share/pandoc/lua-filters/stretch.lua
+++ pandoc/.local/share/pandoc/lua-filters/stretch.lua
@@ -6,11 +6,24 @@ works on *direct* descendants of the heading.
 Since we add the .r-stretch class here, it
 doesn't need to be added manually in org-mode
 (so you can just use a bare image).
+
+If you don't want a given image to be stretched,
+add the "nostretch" attribute to it.
+Org syntax:
+#+ATTR_HTML: :nostretch
+(The actual value of the attribute is ignored,
+ so it can be anything.)
 ]]
 function Para(p)
-  local el = p.c[1]
-  if el.t == "Image" then
-    table.insert(el.classes, "r-stretch")
-    return el
+  local el = p.content[1]
+  if el.tag == "Image" then
+    if el.attributes.nostretch then
+      -- Remove flag as it is no longer needed
+      el.attributes.nostretch = nil
+      return p
+    else
+      table.insert(el.classes, "r-stretch")
+      return el
+    end
   end
 end
