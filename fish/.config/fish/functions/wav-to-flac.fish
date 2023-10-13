ccb563b6395a3fd52358dcf5ab9c9506d59582ae
Goderich <yuhsien77@gmail.com>
2023-10-13 22:05:39 +0800

    Parallelize wav-to-flac conversion

--- fish/.config/fish/functions/wav-to-flac.fish
+++ fish/.config/fish/functions/wav-to-flac.fish
@@ -1,6 +1,10 @@
 function wav-to-flac
-  for wav in *.{WAV,wav}
-    set flac (string replace -ri 'wav$' 'flac' $wav)
-    ffmpeg -i $wav $flac
-  end
+  # use fd for parallelization
+  echo "Converting" (count **/*.{wav,WAV}) "files..."
+  fd --type f --extension wav --exec \
+      ffmpeg -i "{}" \
+      # run quietly, but show conversion progress
+      -loglevel warning \
+      -stats \
+      "{.}.flac"
 end
