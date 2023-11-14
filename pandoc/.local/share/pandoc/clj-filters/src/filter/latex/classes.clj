c978ab332af120ac904316a7b7a5b53046a1de19
Goderich <yuhsien77@gmail.com>
2023-11-14 22:41:33 +0800

    pandoc-clj: add header classes parse filter

--- pandoc/.local/share/pandoc/clj-filters/src/filter/latex/classes.clj
+++ pandoc/.local/share/pandoc/clj-filters/src/filter/latex/classes.clj
@@ -1,50 +1,46 @@
-(ns filter.latex.classes)
+(ns filter.latex.classes
+  (:require [lib.pandoc :as pandoc]))
 
+(defn- span->tag
+  "Extract a tag name from a Span pandoc object.
+  A typical Span object looks like this:
+  {:t \"Span\",
+   :c [[\"\" [\"tag\"] [[\"tag-name\" \"center\"]]]             ;; Attributes
+       [{:t \"SmallCaps\", :c [{:t \"Str\", :c \"center\"}]}]]} ;; Inlines
+  The Haskel type signature is: Span Attr [Inline]
 
-(comment
+  We can extract the tag name from the Attributes or the Inlines,
+  it does not seem to matter which.
+  There is just the one key-value pair per tag.
+  Here I am going with the Attributes."
+  [el]
+  (let [[_ _ [[_ tag]]] (pandoc/attributes el)]
+    tag))
 
-  (defn header-tags [header]
-    (let [inlines (get-in header [:c 2])
-          tag? (fn [el] (= "tag" (get-in el [:c 0 1 0])))]
-      (->> inlines
-           (filterv tag?)
-           (mapv #(get-in % [:c 0 2 0 1])))))
+(defn- extract-tags
+  "Return a seq of :tags: from an org Header."
+  [h]
+  (->> (pandoc/inlines h)
+       (filter pandoc/span?)
+       (map span->tag)))
 
-  (def data (json/decode (slurp "test.json") true))
+(defn- extract-title
+  "Return the title of an org Header (minus tags) as a JSON object.
+  The last object before the first tag (Span) is always a Space,
+  so we remove it."
+  [h]
+  (->> (pandoc/inlines h)
+       (take-while (complement pandoc/span?))
+       butlast
+       vec))
 
-  (defn- extract-title
-    "Return the title of an org Header (minus tags) as a JSON object."
-    [h]
-    (->> (get-in h [:c 2])
-         (take-while (fn [el] (not= "Span" (:t el))))
-         butlast
-         vec))
-
-  (defn- extract-tags
-    "Return a seq of :tags: from an org Header."
-    [h]
-    (->> (get-in h [:c 2])
-         (filter (fn [el] (= "Span" (:t el))))
-         (map (fn [el] (get-in el [:c 0 2 0 1])))))
-
-
-  (let [h (get-in data [:blocks 0])
-        title (extract-title h)
-        tags (extract-tags h)]
+(defn header-parse-tags
+  "Parse the :tags: in an org header into Pandoc classes in the Header object."
+  [h]
+  (let [title (extract-title h)
+        tags (extract-tags h)
+        attrs (-> (pandoc/attributes h)
+                  (update-in [1] (fn [classes] (into classes tags))))]
     (-> h
-        (assoc-in [:c 2] title)
-        (update-in [:c 1 1] (fn [classes] (into classes tags))))
-    )
-  ;; => {:t "Header",
-  ;;     :c
-  ;;     [1
-  ;;      ["introduction-with-four-words" ["animate" "moar"] [["center" "t"]]]
-  ;;      [{:t "Str", :c "Introduction"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "With"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "Four"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "Words"}]]}
-
-  )
+        (pandoc/assoc-inlines title)
+        (pandoc/assoc-attributes attrs))))
