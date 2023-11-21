574e2ba0b170d16aa8c883b1bca274f73df4d737
Goderich <yuhsien77@gmail.com>
2023-11-21 21:16:45 +0800

    pandoc-clj: add stretch to reveal filters

--- pandoc/.local/share/pandoc/clj-filters/src/core/reveal.clj
+++ pandoc/.local/share/pandoc/clj-filters/src/core/reveal.clj
@@ -3,10 +3,13 @@
   (:require [cheshire.core :as json]
             [clojure.walk :refer [prewalk]]
             [filter.classes :as classes]
-            [filter.reveal.bg-img :as bg-img]))
+            [filter.reveal.bg-img :as bg-img]
+            [filter.reveal.stretch :as stretch]))
 
 (def filters
-  (comp bg-img/bg-image classes/header-parse-tags))
+  (comp stretch/stretch
+        bg-img/bg-image
+        classes/header-parse-tags))
 
 (->> (json/decode (slurp *in*) true)
      (prewalk filters)
