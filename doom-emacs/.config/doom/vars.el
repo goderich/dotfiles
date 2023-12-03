d7ac7854647aadb5833ccba9b69295bbd9112895
Goderich <yuhsien77@gmail.com>
2023-12-03 20:43:35 +0800

    doom: restructure vars.el
    Preparing for a full migration to syncthing, which will have the synced
    dirs in various places on various machines.

--- doom-emacs/.config/doom/vars.el
+++ doom-emacs/.config/doom/vars.el
@@ -2,23 +2,8 @@
 
 ;; Define variables for various machines
 
-(defun choose-var-helper (vars)
-  "Comb VARS for three variables: VAR, X, and Y.
-Assign X to VAR if on laptop, otherwise assign Y to VAR."
-  (let ((var (cl-first vars))
-        (x (cl-second vars))
-        (y (cl-third vars)))
-    `(defvar ,var
-       (pcase (system-name)
-         ("iwaka-thinkpad" ,x)
-         (_ ,y)))))
-
-(defmacro choose-vars (lst)
-  "Use X if on laptop, otherwise Y.
-Takes a list of (VAR X Y) lists as an argument."
-  (macroexp-progn (mapcar #'choose-var-helper lst)))
-
-(setq my/org-directory "~/Dropbox/org"
-      my/default-bibliography "~/Dropbox/pap.bib"
-      my/ebib-notes "~/Dropbox/org/literature-notes"
-      my/ebib-file-search-dir "~/Dropbox/ebib")
+(when (string= (system-name) "iwaka-thinkpad")
+  (setq my/org-directory "~/Dropbox/org"
+        my/default-bibliography "~/Dropbox/pap.bib"
+        my/ebib-notes "~/Dropbox/org/literature-notes"
+        my/ebib-file-search-dir "~/files/syncthing/papers/ebib"))
