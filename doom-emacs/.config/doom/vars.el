113dc443fb51a0945c2c5192ce244760cd1d8aef
Goderich <yuhsien77@gmail.com>
2023-12-04 13:59:52 +0800

    doom: Add office comp vars

--- doom-emacs/.config/doom/vars.el
+++ doom-emacs/.config/doom/vars.el
@@ -2,8 +2,16 @@
 
 ;; Define variables for various machines
 
+;; Laptop
 (when (string= (system-name) "iwaka-thinkpad")
   (setq my/org-directory "~/Dropbox/org"
         my/default-bibliography "~/Dropbox/pap.bib"
         my/ebib-notes "~/Dropbox/org/literature-notes"
         my/ebib-file-search-dir "~/files/syncthing/papers/ebib"))
+
+;; Office PC
+(when (string= (system-name) "goderich-ncue")
+  (setq my/org-directory "~/Dropbox/org"
+        my/default-bibliography "~/Dropbox/pap.bib"
+        my/ebib-notes "~/Dropbox/org/literature-notes"
+        my/ebib-file-search-dir "~/files/papers/ebib"))
