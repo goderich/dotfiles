e794988f3b3903e015f21ab3fd5837342408fec2
Goderich <yuhsien77@gmail.com>
2024-05-27 20:51:42 +0800

    pandoc.el: add notifications
    This update adds both Emacs and system notifications that pop up after
    pandoc finishes, and inform if the compilation was successful or not.
    
    I also added an additional variable to the pandoc transient: empty
    pagestyle, for pdf output.
    
    Additionally, I factored out the command string generation out from the
    main function into a helper.

--- doom-emacs/.config/doom/pandoc.el
+++ doom-emacs/.config/doom/pandoc.el
@@ -16,31 +16,39 @@
       (error "Error: more than one CSL file in current directory!"))
     (or (-first-item fs) default)))
 
-(cl-defun gd/pandoc-org--convert (&key extension incremental self-contained numbered)
+(cl-defun gd/pandoc-org--convert (&key extension incremental self-contained numbered empty)
   "Convert the current file using pandoc.
 The format and the defaults file need to be supplied by the caller."
   (save-buffer)
+  (let* ((args (gd/pandoc-org--get-args extension incremental self-contained numbered empty)))
+    (message "Calling: %s" args)
+    (set-process-sentinel
+     (apply #'start-process "pandoc" "*pandoc*" args)
+     #'gd/pandoc-process-sentinel)))
+
+(defun gd/pandoc-org--get-args (extension incremental self-contained numbered empty)
+  "Helper function to construct the correct pandoc call."
   (let* ((input (f-this-file))
          (dir (f-dirname input))
          (output (f-swap-ext input extension))
          (defaults (gd/pandoc--defaults-option extension))
          (metadata (f-join dir "metadata.yaml"))
          (style? (f-exists? (f-join dir "style.css")))
-         (csl (gd/pandoc--find-csl dir))
-         (args `("pandoc" ,input ,defaults
-                 ,@(when (f-exists? metadata) `("--metadata-file" ,metadata))
-                 ,@(when (and (string= extension "html") style?)
-                     '("--css" "./style.css"))
-                 ,@(when (and (string= extension "html") incremental)
-                     '("--incremental=true"))
-                 ,@(when (and (string= extension "html") self-contained)
-                     '("--embed-resources=true" "--standalone"))
-                 ,@(when (and (member extension '("pdf" "docx")) numbered)
-                     '("--number-sections"))
-                 "--csl" ,csl
-                 "-o" ,output)))
-    (message "Calling: %s" args)
-    (apply #'start-process "pandoc" "*pandoc*" args)))
+         (csl (gd/pandoc--find-csl dir)))
+    `("pandoc" ,input ,defaults
+      ,@(when (f-exists? metadata) `("--metadata-file" ,metadata))
+      ,@(when (and (string= extension "html") style?)
+          '("--css" "./style.css"))
+      ,(when (and (string= extension "html") incremental)
+          "--incremental=true")
+      ,@(when (and (string= extension "html") self-contained)
+          '("--embed-resources=true" "--standalone"))
+      ,(when (and (member extension '("pdf" "docx")) numbered)
+          "--number-sections")
+      ,(when (and (string= extension "pdf") empty)
+          "--variable=pagestyle:empty")
+      "--csl" ,csl
+      "-o" ,output)))
 
 (defvar gd/pandoc-org->pdf-hook nil
   "Hook to run before converting from org-mode to PDF.")
@@ -50,8 +58,9 @@ The format and the defaults file need to be supplied by the caller."
 Works only on org files using my pdf template."
   (interactive)
   (run-hooks 'gd/pandoc-org->pdf-hook)
-  (let ((num? (transient-arg-value "number-sections" (transient-args 'gd/pandoc-transient))))
-    (gd/pandoc-org--convert :extension "pdf" :numbered num?)))
+  (let ((num? (transient-arg-value "number-sections" (transient-args 'gd/pandoc-transient)))
+        (empty? (transient-arg-value "empty" (transient-args 'gd/pandoc-transient))))
+    (gd/pandoc-org--convert :extension "pdf" :numbered num? :empty empty?)))
 
 (defun gd/pandoc-org->revealjs ()
   "Convert the current file to revealjs using pandoc.
@@ -91,6 +100,14 @@ Works only on org files using my docx template."
   :init-value (lambda (obj)
                 (oset obj value nil)))
 
+(transient-define-infix gd/pandoc--empty? ()
+  :argument "empty"
+  :shortarg "e"
+  :class 'transient-switch
+  :description "Toggle empty style (no page numbers, pdf only)."
+  :init-value (lambda (obj)
+                (oset obj value nil)))
+
 (transient-define-prefix gd/pandoc-transient ()
   ["Convert this file with pandoc..."
    [("p" "to pdf" gd/pandoc-org->pdf)
@@ -100,4 +117,17 @@ Works only on org files using my docx template."
   ["Options"
    [(gd/pandoc--incremental?)
     (gd/pandoc--self-contained?)
-    (gd/pandoc--number-sections?)]])
+    (gd/pandoc--number-sections?)
+    (gd/pandoc--empty?)]])
+
+(defun gd/pandoc-process-sentinel (process event)
+  "Sentinel for use by this module.
+Sends a notification in Emacs and the system upon completion,
+successful or otherwise."
+  (when (eq (process-status process) 'exit)
+    (if (string-match "finished" event)
+        (progn
+          (shell-command "notify-send 'Pandoc' 'Finished successfully.' --icon=dialog-information --expire-time=10000")
+          (message "Process pandoc finished successfully!"))
+      (shell-command "notify-send 'Pandoc' 'Error, could not compile.' --icon=dialog-information --expire-time=10000")
+      (message "Error: pandoc could not compile!"))))
